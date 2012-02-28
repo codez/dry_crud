9eea78e18b58483ae562040ad6fc2e82850ee9cf
Pascal Zumkehr <z@bluemail.ch>
2012-02-28 21:36:29 +0100

    enhance template, mention it in readme

--- template.rb
+++ template.rb
@@ -1,11 +1,19 @@
+# add gem to Gemfile
 gem 'dry_crud'
-run 'bundle update dry_crud'
 
-if yes?("Use HAML instead of ERB?")
-  generate 'dry_crud', '-t HAML'
+# install gem if missing
+out = run "gem list dry_crud", :capture => true
+run 'bundle update dry_crud' if out !~ /dry_crud/
+
+# generate dry_crud with erb or haml
+templates = ask("Which template engine do you use? [ERB|haml]")
+if "haml".start_with?(templates.downcase)
+  gem 'haml'
+  generate 'dry_crud', '-t haml'
 else
   generate 'dry_crud'
 end
 
+# remove gem from Gemfile
 gsub_file 'Gemfile', /gem .dry_crud./, ""
 
