bacff00c07a9a525e88de53bcc10d6465e4b51ec
Pascal Zumkehr <spam@codez.ch>
2013-03-11 17:06:03 +0100

    fix failing specs

--- lib/generators/dry_crud/templates/spec/support/crud_controller_test_helper.rb
+++ lib/generators/dry_crud/templates/spec/support/crud_controller_test_helper.rb
@@ -20,17 +20,17 @@ module CrudControllerTestHelper
       @@current_stack ||= nil
       if stack == @@current_stack && described_class == @@current_controller.class
         @response = @@current_response
-        @templates = @@current_templates
+        @_templates = @@current_templates
         @controller = @@current_controller
         @request = @@current_request
       else
         perform_request
-        
+
         @@current_stack = stack
         @@current_response = @response
         @@current_request = @request
         @@current_controller = @controller
-        @@current_templates = @templates
+        @@current_templates = @_templates
       end
     else
       perform_request
@@ -52,54 +52,54 @@ module CrudControllerTestHelper
     end
     params
   end
-  
+
   module ClassMethods
-    
+
     # Describe a certain action and provide some usefull metadata.
     # Tests whether this action is configured to be skipped.
     def describe_action(method, action, metadata = {}, &block)
-      describe("#{method.to_s.upcase} #{action}", 
-               {:if => described_class.instance_methods.collect(&:to_s).include?(action.to_s), 
-                :method => method, 
+      describe("#{method.to_s.upcase} #{action}",
+               {:if => described_class.instance_methods.collect(&:to_s).include?(action.to_s),
+                :method => method,
                 :action => action}.
-               merge(metadata), 
+               merge(metadata),
                &block)
     end
-    
+
     def skip?(options, *contexts)
       options ||= {}
       contexts = Array(contexts).flatten
       skips = Array(options[:skip])
       skips = [skips] if skips.blank? || !skips.first.is_a?(Array)
-      
-      skips.flatten.present? && 
+
+      skips.flatten.present? &&
       skips.any? { |skip| skip == contexts.take(skip.size) }
     end
-    
+
     # Test the response status, default 200.
     def it_should_respond(status = 200)
       its(:status) { should == status }
     end
-    
+
     # Test that entries are assigned.
     def it_should_assign_entries
       it "should assign entries" do
         entries.should be_present
       end
     end
-    
+
     # Test that entry is assigned.
     def it_should_assign_entry
       it "should assign entry" do
         entry.should == test_entry
       end
     end
-    
-    # Test that the given template or the main template of the action under test is rendered. 
+
+    # Test that the given template or the main template of the action under test is rendered.
     def it_should_render(template = nil)
       it { should render_template(template || example.metadata[:action]) }
     end
-    
+
     # Test that test_entry_attrs are set on entry.
     def it_should_set_attrs(action = nil)
       it "should set params as entry attributes" do
@@ -111,22 +111,22 @@ module CrudControllerTestHelper
         actual.should == attrs
       end
     end
-    
+
     # Test that the response redirects to the index action.
     def it_should_redirect_to_index
-      it { should redirect_to scope_params.merge(:action => 'index', :returning => true) } 
+      it { should redirect_to scope_params.merge(:action => 'index', :returning => true) }
     end
-    
+
     # Test that the response redirects to the show action of the current entry.
     def it_should_redirect_to_show
-      it { should redirect_to scope_params.merge(:action => 'show', :id => entry.id) } 
+      it { should redirect_to scope_params.merge(:action => 'show', :id => entry.id) }
     end
-    
+
     # Test that the given flash type is present.
     def it_should_have_flash(type, message = nil)
       context "flash" do
         subject { flash }
-        
+
         its([type]) do
           should(message ? match(message) : be_present)
         end
@@ -140,21 +140,21 @@ module CrudControllerTestHelper
         its([type]) { should be_blank }
       end
     end
-    
+
     # Test that the current entry is persistend and valid, or not.
     def it_should_persist_entry(bool = true)
       context "entry" do
         subject { entry }
-    
+
         if bool
-          it { should be_persisted }
+          it { should_not be_new_record }
           it { should be_valid }
         else
-          it { should_not be_persisted }
+          it { should be_new_record }
         end
       end
     end
   end
-  
+
 end
 
