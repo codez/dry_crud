5dd2e175ab27df5cdc356b6166f03a0c66264d25
Pascal Zumkehr <spam@codez.ch>
2019-09-04 21:26:41 +0200

    version bump to 6.0

--- lib/dry_crud/engine.rb
+++ lib/dry_crud/engine.rb
@@ -1,14 +1,17 @@
 module DryCrud
   # Dry Crud Rails engine
   class Engine < Rails::Engine
+
+    # Fields with errors are directly styled in DryCrud::FormBuilder.
+    # Rails should just output the plain html tag.
     initializer 'dry_crud.field_error_proc' do |_app|
-      # Fields with errors are directly styled in DryCrud::FormBuilder.
-      # Rails should just output the plain html tag.
       ActionView::Base.field_error_proc =
         proc { |html_tag, _instance| html_tag }
+    end
 
-      # Load dry_crud engine helpers first so that the application may override
-      # them.
+    # Load dry_crud engine helpers first so that the application may override
+    # them.
+    config.to_prepare do
       paths = ApplicationController.helpers_path
       helper_path = "#{File::SEPARATOR}app#{File::SEPARATOR}helpers"
       regexp = /dry_crud(-\d+\.\d+\.\d+)?#{helper_path}\z/
@@ -18,5 +21,6 @@ module DryCrud
         paths.prepend(dry_crud_helpers)
       end
     end
+
   end
 end
